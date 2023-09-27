8393314ade402262def71e337a28be8c6aeb6a4d
GamayMaster <110918831+GamayMaster@users.noreply.github.com>
2023-09-27 07:04:59 -0400

    Add files via upload

---
+++ ADC.h
@@ -0,0 +1,16 @@
+/*
+ * ADC.h
+ *
+ *  Created on: sept 25, 2023
+ *      Author: Ahmed Gamal
+ */
+
+#ifndef ADC_H
+#define ADC_H
+
+#include "macro.h"
+
+void ADC_Init(void);
+word ADC_Read(void);
+
+#endif
